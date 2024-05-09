755f079cf4c7becd2e28a100c6becf4b211fa6bd
Alejandro Sánchez Fernández <alexius1996@gmail.com>
2024-05-09 22:42:35 +0200

    Refactor API routes

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -11,8 +11,8 @@ use App\Http\Controllers\API\EventController;
 use App\Http\Controllers\API\FollowerController;
 
 // Public routes
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']); //register a new user using name, email password and password_confirmation
+Route::post('/login', [AuthController::class, 'login']); //login a user using email and password return a token to authenticate in the private routes
 
 // Private routes for users
 Route::middleware(['auth:sanctum'])->group(function () {
@@ -20,7 +20,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     // ------------------------------------------------------------------ USER ENDPOINTS ------------------------------------------------------------------
 
     Route::post('/logout', [AuthController::class, 'logout']); //logout the user authenticated
-    Route::get('/users', [UserController::class, 'index']); //name, email and id from all users
+    Route::get('/users', [UserController::class, 'index']); //return name, email and id from all users
+    
     Route::get('/user/{id}', [UserController::class, 'show']); //all data from specific user
     Route::get('/user', [UserController::class, 'user']); //return all data from the authenticated user
     

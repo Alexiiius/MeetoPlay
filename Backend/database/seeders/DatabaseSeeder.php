131593af1035030475780668825711009cbca029
Alejandro Sánchez Fernández <alejandro.sanchez@viewnext.com>
2024-05-14 12:27:55 +0200

    Add EventFactory and EventUserFactory. Added faker datas for events, and participants.

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use App\Models\Event;
 use App\Models\EventRequirement;
 use App\Models\Follower;
+use App\Models\Participant;
+use App\Models\EventUser;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,7 +19,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        User::factory(30)->create();
         User::factory()->create([
             'name' => 'admin',
             'email' => 'admin@admin.com',
@@ -25,7 +27,21 @@ class DatabaseSeeder extends Seeder
             'is_Admin' => true,
         ]);
 
-        Follower::factory(10)->create();
+        Follower::factory(30)->create();
+        $events = Event::factory(1)->create(['event_requirement_id' => null]);
+        $event = $events[0];
+        $eventRequirement = EventRequirement::create([
+            'event_id' => 1,
+            'max_level' => '10',
+            'min_level' => '1',
+            'max_rank' => 'Diamond',
+            'min_rank' => 'Iron',
+            'min_hours_played' => 100,
+            'max_hours_played' => 1000,
+        ]);
+        $event->update(['event_requirement_id' => $eventRequirement->id]);
+        Event::factory(10)->create();
+        EventUser::factory(50)->create();
 
         
     }

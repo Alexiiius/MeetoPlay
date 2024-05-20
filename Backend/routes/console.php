7986680e6875fe231969cd60f2c695b4095b57be
Alejandro Sánchez Fernández <alexius1996@gmail.com>
2024-05-20 20:30:36 +0200

    added schelude hourly soft delete for passed events

--- Backend/routes/console.php
+++ Backend/routes/console.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+//delete old events
+Artisan::command('delete:old_events', function () {
+    $events = \App\Models\Event::all();
+    foreach ($events as $event) {
+        if ($event->date_time_end < now()) {
+            $event->softDelete();
+        }
+    }
+})->purpose('Delete old events')->hourly();

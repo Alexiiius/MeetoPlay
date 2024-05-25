9da54064eb1f713f497269394ce917e8f6319c5b
DaniRamirezVaquero <dramvaq@gmail.com>
2024-05-25 17:44:51 +0200

    Refactor app.routes.ts to remove commented code and update route paths

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -8,44 +8,12 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { GameStatsComponent } from './components/profile/game-stats/game-stats.component';
 import { MyEventsComponent } from './components/profile/my-events/my-events.component';
 import { ParticipatingComponent } from './components/profile/participating/participating.component';
-import { EventFormComponent } from './components/event-form/event-form.component';
-
-// export const routes: Routes = [
-//   { path: '', redirectTo: 'main', pathMatch: 'full' },
-//   {
-//     path: 'main', component: MainComponent, canActivate: [authGuard], children: [
-//       { path: '', redirectTo: 'profile/1', pathMatch: 'full' },
-//       {
-//         path: 'profile/:id', component: ProfileComponent, children: [
-//           { path: '', redirectTo: 'myEvents', pathMatch: 'full' },
-//           { path: 'gameStats', component: GameStatsComponent },
-//           { path: 'myEvents', component: MyEventsComponent },
-//           { path: 'participating', component: ParticipatingComponent },
-//         ]
-//       },
-//       {
-//         path: 'newEvent', component: NewEventFormComponent, children: [
-//           { path: '', redirectTo: 'what', pathMatch: 'full' },
-//           { path: 'what', component: WhatFormComponent, data: { animation: 'whatAnim' } },
-//           { path: 'when', component: WhenFormComponent, data: { animation: 'whenAnim' } },
-//           { path: 'who', component: WhoFormComponent, data: { animation: 'whoAnim' } },
-//           // otras rutas hijas aquí
-//         ]
-//       },
-//       // otras rutas hijas aquí
-//     ]
-//   },
-//   { path: 'login', component: LoginRegisterComponent, canActivate: [loggedGuard], data: { mode: 'login' } },
-//   { path: 'register', component: LoginRegisterComponent, canActivate: [loggedGuard], data: { mode: 'register' } },
-//   // otras rutas aquí
-//   { path: '**', redirectTo: 'main' },
-// ];
 
 export const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' },
   {
     path: 'main', component: MainComponent, canActivate: [authGuard], children: [
-      { path: '', component: EventsFeedComponent }, // TODO cambiar a EventsFeedComponent
+      { path: '', component: EventsFeedComponent },
       {
         path: 'profile/:id', component: ProfileComponent, children: [
           { path: '', redirectTo: 'gameStats', pathMatch: 'full' },
@@ -60,16 +28,5 @@ export const routes: Routes = [
   { path: 'register', component: LoginRegisterComponent, canActivate: [loggedGuard], data: { mode: 'register' } },
 
   // otras rutas aquí
-  { path: '**', redirectTo: 'wip' },
+  { path: '**', redirectTo: 'main' },
 ];
-
-// {
-//   path: 'newEvent',
-//   component: NewEventFormComponent,
-//   children: [
-//     { path: '', redirectTo: 'what', pathMatch: 'full' },
-//     { path: 'what', component: WhatFormComponent, data: { animation: 'whatAnim' } },
-//     { path: 'when', component: WhenFormComponent, data: { animation: 'whenAnim' } },
-//     { path: 'who', component: WhoFormComponent, data: { animation: 'whoAnim' } },
-//   ]
-// },

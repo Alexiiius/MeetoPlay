38c4f34a9fcc96e8c6dd344d43955dc05dd31291
Alejandro Sanchez Fernandez <alejandro.sanchez@viewnext.com>
2024-06-03 09:31:58 +0200

    WIP admin blade views

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -4,21 +4,21 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Carbon;
 
 Route::get('health-check-json', function () {
-    return response()->json([ 'status' => 'OK', 'timestamp' => Carbon::now() ]);
+    return response()->json([ 'status' => 'OK', 'timestamp' => now()->format('d-m-Y\TH:i:s. T') ]);
 });
 
 Route::get('health-check', function () {
     return "LARAVEL 11 IS A HELL";
 });
 
-Route::view('/', 'welcome');
+// Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+// Route::view('profile', 'profile')
+//     ->middleware(['auth'])
+//     ->name('profile');
 
 require __DIR__.'/auth.php';

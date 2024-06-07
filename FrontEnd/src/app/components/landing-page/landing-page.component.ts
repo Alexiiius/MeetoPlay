fd5681708dcdf4b1076354a8fcac302bd0b6e14d
DaniRamirezVaquero <dramvaq@gmail.com>
2024-06-07 21:32:57 +0200

    landing finished, pending last check

--- FrontEnd/src/app/components/landing-page/landing-page.component.ts
+++ FrontEnd/src/app/components/landing-page/landing-page.component.ts
@@ -1,11 +1,13 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, ViewportScroller } from '@angular/common';
 import { Component, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
@@ -13,7 +15,10 @@ import { Component, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/co
 export class LandingPageComponent implements OnInit, OnDestroy{
   isSideMenuOpen: boolean = false;
 
-  constructor(private renderer: Renderer2, private el: ElementRef) { }
+  constructor(
+    private renderer: Renderer2,
+    private el: ElementRef,
+    private viewportScroller: ViewportScroller) { }
 
   toggleSideMenu() {
     this.isSideMenuOpen = !this.isSideMenuOpen;
@@ -36,6 +41,15 @@ export class LandingPageComponent implements OnInit, OnDestroy{
     window.removeEventListener('scroll', this.scroll, true);
   }
 
+  scrollToAnchor(anchor: string, sideMenu: boolean = false): void {
+    if (sideMenu) {
+      this.toggleSideMenu();
+    }
+    const element = document.getElementById(anchor);
+    if (element) {
+      element.scrollIntoView({behavior: 'smooth', block: 'center'});
+    }
+  }
   scroll = (event: Event): void => {
     // here you can access the scroll event
     const scrollThreshold = 200; // replace with the amount of pixels you want

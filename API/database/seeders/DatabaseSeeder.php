ae84c1fa0f7a3c91e75f5d8a61d5210ef82bee8d
Daniel Ramírez Vaquero <dramvaq@gmail.com>
2024-06-14 10:50:43 +0200

    seeded external api

--- API/database/seeders/DatabaseSeeder.php
+++ API/database/seeders/DatabaseSeeder.php
@@ -43,7 +43,7 @@ class DatabaseSeeder extends Seeder
             ['platform' => 'Crossplay'],
             ['platform' => 'Other'],
         ]);
-        
+
 
         Game::factory()->create([
             'name' => 'League of Legends',
@@ -113,7 +113,7 @@ class DatabaseSeeder extends Seeder
             'description' => 'Game mode in which you play with a random champion on a single lane map',
             'game_id' => 1,
             'scenario_name' => json_encode(['Summoners Rift']),
-            'ranks' => json_encode(['']),
+            'ranks' => json_encode(['Iron', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Emerald', 'Diamond', 'Master', 'Grandmaster', 'Challenger']),
             'min_players' => 5,
             'max_players' => 5,
             'ranked' => true,
@@ -218,14 +218,391 @@ class DatabaseSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        
+        // ----------------------------------------------------------------------------------------
+
+        Game::factory()->create([
+            'name' => 'Apex Legends',
+            'description' => '',
+            'nickname_name' => json_encode(['nick', 'nickname', 'alias', 'username']),
+            'account_level_name' => '',
+            'image' => 'https://meetoplay-api-assets-bucket.s3.amazonaws.com/gameLogos/Apex_Legends.png',
+        ]);
+
+        GameMode::factory()->create([
+            'name' => 'Battle Royale - Trios',
+            'description' => '',
+            'game_id' => 3,
+            'scenario_name' => json_encode(['']),
+            'ranks' => json_encode(['']),
+            'min_players' => 1,
+            'max_players' => 3,
+            'ranked' => false,
+        ]);
+
+        GameMode::factory()->create([
+            'name' => 'Battle Royale - Solo',
+            'description' => '',
+            'game_id' => 3,
+            'scenario_name' => json_encode(['']),
+            'ranks' => json_encode(['']),
+            'min_players' => 1,
+            'max_players' => 1,
+            'ranked' => false,
+        ]);
 
 
+        GameMode::factory()->create([
+            'name' => 'Ranked League',
+            'description' => '',
+            'game_id' => 3,
+            'scenario_name' => json_encode(['']),
+            'ranks' => json_encode(['Rookie', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Master', 'Apex Predator']),
+            'min_players' => 1,
+            'max_players' => 3,
+            'ranked' => true,
+        ]);
 
-        
-        
+        GameMode::factory()->create([
+            'name' => 'Mixtape',
+            'description' => '',
+            'game_id' => 3,
+            'scenario_name' => json_encode(['']),
+            'ranks' => json_encode(['']),
+            'min_players' => 1,
+            'max_players' => 3,
+            'ranked' => false,
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 3,
+            'platform_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 3,
+            'platform_id' => 2,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 3,
+            'platform_id' => 3,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 3,
+            'platform_id' => 4,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 3,
+            'platform_id' => 5,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 3,
+            'platform_id' => 7,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
 
 
+        // ----------------------------------------------------------------------------------------
 
+
+        Game::factory()->create([
+            'name' => 'Overwatch',
+            'description' => '',
+            'nickname_name' => json_encode(['nick', 'nickname', 'alias', 'username']),
+            'account_level_name' => '',
+            'image' => 'https://meetoplay-api-assets-bucket.s3.amazonaws.com/gameLogos/Overwatch.png',
+        ]);
+
+        GameMode::factory()->create([
+            'name' => 'Quick Play',
+            'description' => '',
+            'game_id' => 4,
+            'scenario_name' => json_encode(['']),
+            'ranks' => json_encode(['']),
+            'min_players' => 1,
+            'max_players' => 6,
+            'ranked' => false,
+        ]);
+
+        GameMode::factory()->create([
+            'name' => 'Competitive game',
+            'description' => '',
+            'game_id' => 4,
+            'scenario_name' => json_encode(['']),
+            'ranks' => json_encode(['Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Master', 'Grandmaster', 'Top 500']),
+            'min_players' => 1,
+            'max_players' => 6,
+            'ranked' => true,
+        ]);
+
+        GameMode::factory()->create([
+            'name' => 'Arcade',
+            'description' => '',
+            'game_id' => 4,
+            'scenario_name' => json_encode(['']),
+            'ranks' => json_encode(['']),
+            'min_players' => 1,
+            'max_players' => 6,
+            'ranked' => false,
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 4,
+            'platform_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 4,
+            'platform_id' => 2,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 4,
+            'platform_id' => 3,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 4,
+            'platform_id' => 4,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        //----------------------------------------------------------------------------------------
+
+        Game::factory()->create([
+            'name' => 'Among Us',
+            'description' => '',
+            'nickname_name' => json_encode(['nick', 'nickname', 'alias', 'username']),
+            'account_level_name' => '',
+            'image' => 'https://meetoplay-api-assets-bucket.s3.amazonaws.com/gameLogos/Among_Us.png',
+        ]);
+
+        GameMode::factory()->create([
+            'name' => 'Public',
+            'description' => '',
+            'game_id' => 5,
+            'scenario_name' => json_encode(['The Skeld', 'Mira HQ']),
+            'ranks' => json_encode(['']),
+            'min_players' => 4,
+            'max_players' => 10,
+            'ranked' => false,
+        ]);
+
+        GameMode::factory()->create([
+            'name' => 'Private',
+            'description' => '',
+            'game_id' => 5,
+            'scenario_name' => json_encode(['The Skeld', 'Mira HQ']),
+            'ranks' => json_encode(['']),
+            'min_players' => 4,
+            'max_players' => 10,
+            'ranked' => false,
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 5,
+            'platform_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 5,
+            'platform_id' => 2,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 5,
+            'platform_id' => 3,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 5,
+            'platform_id' => 4,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 5,
+            'platform_id' => 5,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+
+        //----------------------------------------------------------------------------------------
+
+        Game::factory()->create([
+            'name' => 'Fortnite',
+            'description' => '',
+            'nickname_name' => json_encode(['nick', 'nickname', 'alias', 'username']),
+            'account_level_name' => '',
+            'image' => 'https://meetoplay-api-assets-bucket.s3.amazonaws.com/gameLogos/Fortnite.png',
+        ]);
+
+        GameMode::factory()->create([
+            'name' => 'Battle Royale',
+            'description' => '',
+            'game_id' => 6,
+            'scenario_name' => json_encode(['']),
+            'ranks' => json_encode(['']),
+            'min_players' => 1,
+            'max_players' => 3,
+            'ranked' => false,
+        ]);
+
+        GameMode::factory()->create([
+            'name' => 'Creative Sandbox',
+            'description' => '',
+            'game_id' => 6,
+            'scenario_name' => json_encode(['']),
+            'ranks' => json_encode(['']),
+            'min_players' => 1,
+            'max_players' => 16,
+            'ranked' => false,
+        ]);
+
+        GameMode::factory()->create([
+            'name' => 'Save the World',
+            'description' => '',
+            'game_id' => 6,
+            'scenario_name' => json_encode(['']),
+            'ranks' => json_encode(['']),
+            'min_players' => 1,
+            'max_players' => 4,
+            'ranked' => false,
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 6,
+            'platform_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 6,
+            'platform_id' => 2,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 6,
+            'platform_id' => 3,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 6,
+            'platform_id' => 4,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 6,
+            'platform_id' => 5,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 6,
+            'platform_id' => 7,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        //----------------------------------------------------------------------------------------
+
+        Game::factory()->create([
+            'name' => 'Valorant',
+            'description' => '',
+            'nickname_name' => json_encode(['nick', 'nickname', 'alias', 'username']),
+            'account_level_name' => '',
+            'image' => 'https://meetoplay-api-assets-bucket.s3.amazonaws.com/gameLogos/Valorant+2.png',
+        ]);
+
+        GameMode::factory()->create([
+            'name' => 'Unrated',
+            'description' => '',
+            'game_id' => 7,
+            'scenario_name' => json_encode(['']),
+            'ranks' => json_encode(['']),
+            'min_players' => 1,
+            'max_players' => 5,
+            'ranked' => false,
+        ]);
+
+        GameMode::factory()->create([
+            'name' => 'Competitive',
+            'description' => '',
+            'game_id' => 7,
+            'scenario_name' => json_encode(['']),
+            'ranks' => json_encode(['Iron', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Immortal', 'Radiant']),
+            'min_players' => 1,
+            'max_players' => 5,
+            'ranked' => true,
+        ]);
+
+        GameMode::factory()->create([
+            'name' => 'Spike Rush',
+            'description' => '',
+            'game_id' => 7,
+            'scenario_name' => json_encode(['']),
+            'ranks' => json_encode(['']),
+            'min_players' => 1,
+            'max_players' => 5,
+            'ranked' => false,
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 7,
+            'platform_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 7,
+            'platform_id' => 2,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('game_platform')->insert([
+            'game_id' => 7,
+            'platform_id' => 3,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
